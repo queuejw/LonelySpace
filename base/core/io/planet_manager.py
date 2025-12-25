d2b13dcd185fe73a62f688bd0d5760e1677890cd
queuejw <dimon6018t@gmail.com>
2025-12-25 21:08:12 +0400

    Исправлена загрузка игры при отсутствии событий на планете

--- base/core/io/planet_manager.py
+++ base/core/io/planet_manager.py
@@ -9,10 +9,11 @@ from base.game.classes.planet.planet_event import PlanetEvent
 
 # Загружает список планет из json файла и возвращает его.
 def load_planets(path: str) -> list[Planet]:
-
     # Возвращает список с событиями на планете.
     def load_events_list(d: list[dict]) -> list[PlanetEvent]:
-        return [PlanetEvent(item['name'], item['description'], list(item['commands']), float(item['prob']), item['color']) for item in d]
+        return [
+            PlanetEvent(item['name'], item['description'], list(item['commands']), float(item['prob']), item['color'])
+            for item in d]
 
     if components.SETTINGS.get_debug_mode():
         print(f"Попытка загрузить планеты из файла {path}")
@@ -23,18 +24,20 @@ def load_planets(path: str) -> list[Planet]:
             if components.SETTINGS.get_debug_mode():
                 print(f"Планеты успешно загружены, создание списка ...")
             custom_planet = path == constants.CUSTOM_PLANETS_FILE_PATH
+            gen_list = []
+            for m in planets:
+                try:
+                    planet = Planet(m['id'], m['name'], m['description'], m['type'], m['danger'], m['eta'],
+                                    m['temperature'], load_events_list(m['events']),
+                                    custom_planet, constants.PRODUCT_NAME if not custom_planet else m['author'])
+                    gen_list.append(planet)
+                    del planet
+                except KeyError as e:
+                    print(f"{colorama.Fore.RED}Ошибка при загрузке планеты: {e}")
 
-            generated_list = [
-                Planet(m['id'], m['name'], m['description'], m['type'], m['danger'], m['eta'], m['temperature'], load_events_list(m['events']),
-                       custom_planet, constants.PRODUCT_NAME if not custom_planet else m['author']) for m
-                in planets]
-            return generated_list
+            return gen_list
     except FileNotFoundError:
         print(
             f"{colorama.Fore.RED}[E] Файл {path}, который должен содержать планеты, не найден. Невозможно продолжить работу.")
         components.ENGINE.running = False
-        return []
-    except KeyError as e:
-        print(
-            f"{colorama.Fore.RED}[E] Файл {path}, который должен содержать планеты, содержит ошибки. Восстановите файл, либо свяжитесь с нами.\n\nИнформация: {e}")
         return []
